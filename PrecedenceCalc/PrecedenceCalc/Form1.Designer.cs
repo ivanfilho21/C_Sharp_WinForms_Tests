6b68c9df4d6b743a3359b3333b04230ec202d7ef
ivanfilho21 <ivanfilho21@gmail.com>
2019-01-22 12:43:16 -0300

    First commit.

---
+++ PrecedenceCalc/PrecedenceCalc/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace PrecedenceCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.doButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Expression:";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(12, 32);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(326, 26);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Result:";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.resultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(12, 125);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(326, 66);
+            this.resultLabel.TabIndex = 3;
+            // 
+            // doButton
+            // 
+            this.doButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.doButton.Location = new System.Drawing.Point(135, 68);
+            this.doButton.Name = "doButton";
+            this.doButton.Size = new System.Drawing.Size(75, 23);
+            this.doButton.TabIndex = 4;
+            this.doButton.Text = "Calculate";
+            this.doButton.UseVisualStyleBackColor = true;
+            this.doButton.Click += new System.EventHandler(this.DoButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.doButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 200);
+            this.Controls.Add(this.doButton);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(366, 238);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button doButton;
+    }
+}
+
---
+++ PrecedenceCalc/PrecedenceCalc/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PrecedenceCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DoButton_Click(object sender, EventArgs e)
+        {
+            Decode();
+        }
+
+        private void Decode()
+        {
+            string times = "*", obelus = "/", plus = "+", minus = "-";
+            string txt = inputTextBox.Text;
+
+            StringBuilder sb = new StringBuilder();
+            // Low precedence symbols
+            char[] lpRegex = sb.Append("[").Append(plus).Append(minus).Append("]").ToString().ToCharArray();
+            sb = new StringBuilder();
+            // High precedence symbols
+            char[] hpRegex = sb.Append("[").Append(times).Append(obelus).Append("]").ToString().ToCharArray();
+
+            string[] exp = null;
+
+            // 2 * 1 + 2 - 3
+            // Debugging
+            resultLabel.Text = "";
+
+            // tentar um loop
+            for ( ; ; )
+            {
+                if (ContainsRegex(txt, lpRegex))
+                {
+                    string[] txtSplit = txt.Split(lpRegex);
+                    if (txtSplit.Length <= 1) return;
+
+                    for (var j = 0; j < txtSplit.Length; j++)
+                    {
+                        if (ContainsRegex(txtSplit[j], lpRegex)) ;
+                    }
+
+
+                }
+                else if (ContainsRegex(txt, hpRegex))
+                {
+                    // caso não possua - ou +, ...
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
+            if ( ContainsRegex(txt, lpRegex) )
+            {
+                // It could contain one of them, both or even all operands.
+                string[] aux = txt.Split(lpRegex);
+
+                // Debugging
+                for (var i = 0; i < aux.Length; i++)
+                    resultLabel.Text += ": " + aux[i] + "\n";
+            }
+            else
+            {
+                // There is only minus, plus or none.
+                
+            }
+        }
+
+        private bool ContainsRegex(string src, char[] regex)
+        {
+            for (var i = 0; i < regex.Length; i++)
+            {
+                if (src.Contains( regex[i].ToString() )) return true;
+            }
+            return false;
+        }
+
+        // Retuns the index of regex
+        private int get(string src, char[] regex)
+        {
+
+            return -1; // no index
+        }
+    }
+}
